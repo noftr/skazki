4d4c383e245288f2dc5259471c328f0b90b38728
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-06-21 23:09:17 +0300

    ease

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -1,9 +1,10 @@
 ScrollTrigger.create({
     trigger: '.main-3s__text-container-1',
-    animation: gsap.fromTo('.main-3s__text-container-1', {scale: 1 }, {scale: 10}), gsap.to('.main-3s__text-container-1', {scale: 10 }, {scale: 1}),
+    animation: gsap.fromTo('.main-3s__text-container-1', {scale: 1 }, {scale: 10}),
     toggleClass: 'active',
     scroller: ".scroll-wrapper",
     start: 'top 60%',
     end: 'top 40%',
+    ease: "power4.out",
     markers: true,
 });
