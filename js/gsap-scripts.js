d51d479df5bd10abc93a382aac2b26b1a73c0b4c
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-06-21 22:59:41 +0300

    toggleClass

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -1,7 +1,7 @@
 ScrollTrigger.create({
     trigger: '.main-3s__text-container-1',
     animation: gsap.fromTo('.main-3s__text-container-1', {scale: 1 }, {scale: 10}),
-    //  toggleClass: 'action',
+    toggleClass: 'action',
     scroller: ".scroll-wrapper",
     start: 'top 60%',
     end: 'top 40%',
