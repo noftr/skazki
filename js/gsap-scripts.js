41a36cd866cbe2d14df1dc6f9f1d995b84171711
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-06-21 23:06:46 +0300

    add gsap.to

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -1,6 +1,6 @@
 ScrollTrigger.create({
     trigger: '.main-3s__text-container-1',
-    animation: gsap.fromTo('.main-3s__text-container-1', {scale: 1 }, {scale: 10}),
+    animation: gsap.fromTo('.main-3s__text-container-1', {scale: 1 }, {scale: 10}), gsap.to('.main-3s__text-container-1', {scale: 10 }, {scale: 1}),
     toggleClass: 'active',
     scroller: ".scroll-wrapper",
     start: 'top 60%',
