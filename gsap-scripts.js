205fa2ee537a0dc5865562d217b1cdf6beffd335
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-06-21 22:25:17 +0300

    прикручиваю тестовый скрипт на сайт

--- gsap-scripts.js
+++ gsap-scripts.js
@@ -1,1 +1,9 @@
-иа
+ScrollTrigger.create({
+    trigger: '.main-3s__text-container-1',
+    animation: gsap.fromTo('.main-3s__text-container-1', {scale: 1 }, {scale: 10}),
+    //  toggleClass: 'action',
+    scroller: ".scroll-wrapper",
+    start: '30% 0%',
+  //  end: '1000% 0%',
+    markers: true,
+});
