3c2dd321926a9e07fb8655ecfc99ca0dd98b95c0
noftr <97229373+noftr@users.noreply.github.com>
2022-06-22 16:39:06 +0300

    Починил анимацию

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -1,12 +1,12 @@
 ScrollTrigger.create({
     trigger: '.main-3s__text-container-1',
     animation:
-    gsap.fromTo('.main-3s__text-container-1', {scale: 1 }, {scale: 10})
-    .fromTo('.main-3s__text-container-1', {backgroundColor: "#ffffff"}, {backgroundColor: "#28a92b"}),
+    gsap.fromTo('.main-3s__text-container-1', {scale: 1 }, {scale: 10}),
     scroller: ".scroll-wrapper",
     start: 'top 55%',
     end: 'bottom 45%',
     ease: "power4.out",
     markers: true,
     toggleActions: "play reverse play reverse",
+    // toggleClass: "active",
 });
