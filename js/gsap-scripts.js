d303bc211a19d78f87a02ae61aa6e738f746ce1c
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-06-23 02:10:24 +0300

    main anim 0.2

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -28,7 +28,7 @@ function onEnterFuncBack1() {
 function onLeaveFunc1() {
     gsap.to('.main-3s__text-container-1',{color: "#FFFFFF", duration: .4, ease: "power3.inOut",});
     gsap.to('.main-3s__number-1',{opacity: 0, delay: .1, duration: .3, ease: "power3.out",});
-    gsap.to(".main-3s__icon-1", {xPercent: 200, duration: .6, ease: "power3.inOut",});
+    gsap.fromTo(".main-3s__icon-1", {xPercent: 0}, {xPercent: 100, duration: .6, ease: "power3.inOut",});
     gsap.fromTo(".main-3s__head-wrapper-1", {yPercent: 0}, {yPercent: -100, duration: 1, ease: "power3.inOut",});
 };
 
