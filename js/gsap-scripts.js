4aa755089beded599713ce1d50d88b2c2705cbc6
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-06-23 01:09:02 +0300

    add inout

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -18,7 +18,7 @@ function onEnterFunc1() {
     gsap.to('.main-3s__text-container-1',{
         scale: 10,
         duration: 1,
-        ease: "power3.out",
+        ease: "power3.inOut",
     });
 };
 
@@ -26,7 +26,7 @@ function onLeaveFunc1() {
     gsap.to('.main-3s__text-container-1',{
         scale: 1,
         duration: 1,
-        ease: "power3.out",
+        ease: "power3.inOut",
     });
 };
 
@@ -36,14 +36,14 @@ function onLeaveFunc1() {
 // gsap.fromTo('.main-3s__icon-1', { scale: 1 }, {
 //     scale: 10,
 //     duration: 1,
-//     ease: "power2.out",
+//     ease: "power2.inOut",
 //     scrollTrigger: {
 //         trigger: '.main-3s__trigger-1',
 //         // animation: animationCard,
 //         scroller: ".scroll-wrapper",
 //         start: 'top 53%',
 //         end: 'bottom 47%',
-//         ease: "power4.out",
+//         ease: "power4.inOut",
 //         markers: true,
 //         toggleActions: "play reverse play reverse",
 //         // toggleClass: "active",
