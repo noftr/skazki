6115a4152b46a49b49c887372f4db0343a304b99
noftr <97229373+noftr@users.noreply.github.com>
2022-06-22 17:08:27 +0300

    put scrolltrigger at from.to

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -1,13 +1,18 @@
-ScrollTrigger.create({
-    trigger: '.main-3s__text-container-1',
-    animation: animationCard,
-    scroller: ".scroll-wrapper",
-    start: 'top 55%',
-    end: 'bottom 45%',
-    ease: "power4.out",
-    markers: true,
-    toggleActions: "play reverse play reverse",
-    toggleClass: "active",
-});
 
-animationCard.fromTo('.main-3s__text-container-1', {scale: 1 }, {scale: 10, duration: 1, ease: "elastic"});
+
+gsap.fromTo('.main-3s__text-container-1', {scale: 1 }, {
+    scale: 10,
+    duration: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: '.main-3s__text-container-1',
+        // animation: animationCard,
+        scroller: ".scroll-wrapper",
+        start: 'top 55%',
+        end: 'bottom 45%',
+        ease: "power4.out",
+        markers: true,
+        toggleActions: "play reverse play reverse",
+        // toggleClass: "active",
+    };
+});
