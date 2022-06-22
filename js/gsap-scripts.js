85c40098051b07506f0f3d0f52e519b09d61c370
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-06-23 01:46:47 +0300

    add default ease 0.1

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -1,4 +1,4 @@
-let defaultEase = CustomEase.create("custom", "M0,0 C0.25,0.1 0.25,1 1,1")
+
 let animTrigger = ScrollTrigger.create({
     trigger: '.main-3s__trigger-1',
     // animation: animationCard,
@@ -17,30 +17,30 @@ function onEnterFunc1() {
     gsap.to('.main-3s__text-container-1',{
         color: "#FFEE50",
         duration: .4,
-        ease: defaultEase,
+        ease: CustomEase.create("custom", "M0,0 C0.25,0.1 0.25,1 1,1"),
     });
     gsap.to('.main-3s__number-1',{
         opacity: 1,
         delay: .1,
         duration: .3,
-        ease: defaultEase,
+        ease: "power3.out",
     });
-    gsap.fromTo(".main-3s__icon-1", {xPercent: -100}, {xPercent: 0, duration: .6, ease: "power3.inOut",});
+    gsap.fromTo(".main-3s__icon-1", {xPercent: -100}, {xPercent: 0, duration: .6, ease: CustomEase.create("custom", "M0,0 C0.25,0.1 0.25,1 1,1"),});
 };
 
 function onLeaveFunc1() {
     gsap.to('.main-3s__text-container-1',{
         color: "#FFFFFF",
         duration: .4,
-        ease: defaultEase,
+        ease: CustomEase.create("custom", "M0,0 C0.25,0.1 0.25,1 1,1"),
     });
     gsap.to('.main-3s__number-1',{
         opacity: 0,
         delay: .1,
         duration: .3,
-        ease: defaultEase,
+        ease: "power3.out",
     });
-    gsap.fromTo(".main-3s__icon-1", {xPercent: 0}, {xPercent: 100, duration: .6, ease: "power3.inOut",});
+    gsap.fromTo(".main-3s__icon-1", {xPercent: 0}, {xPercent: 100, duration: .6, ease: CustomEase.create("custom", "M0,0 C0.25,0.1 0.25,1 1,1"),});
 };
 
 
