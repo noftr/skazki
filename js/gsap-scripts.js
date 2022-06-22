02afe76bfea23819e81ad19b52b20ed366b935c0
noftr <97229373+noftr@users.noreply.github.com>
2022-06-22 18:19:01 +0300

    add trigger

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -5,7 +5,7 @@ gsap.fromTo('.main-3s__text-container-1', {scale: 1 }, {
     duration: 1,
     ease: "power2.out",
     scrollTrigger: {
-        trigger: '.main-3s__text-container-1',
+        trigger: '.main-3s__trigger-1',
         // animation: animationCard,
         scroller: ".scroll-wrapper",
         start: 'top 55%',
