e1ceea4cb2d87738c578c3ccbad1d7783b3c1bb1
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-06-23 00:27:18 +0300

    test let trigger

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -1,18 +1,39 @@
 
+let animTrigger = ScrollTrigger.create({
+    trigger: '.main-3s__trigger-1',
+    // animation: animationCard,
+    scroller: ".scroll-wrapper",
+    start: 'top 53%',
+    end: 'bottom 47%',
+    ease: "power4.out",
+    markers: true,
+    toggleActions: "play reverse play reverse",
+    // toggleClass: "active",
+});
 
-gsap.fromTo('.main-3s__text-container-1', {scale: 1 }, {
+
+gsap.fromTo('.main-3s__text-container-1', { scale: 1 }, {
     scale: 10,
     duration: 1,
     ease: "power2.out",
-    scrollTrigger: {
-        trigger: '.main-3s__trigger-1',
-        // animation: animationCard,
-        scroller: ".scroll-wrapper",
-        start: 'top 53%',
-        end: 'bottom 47%',
-        ease: "power4.out",
-        markers: true,
-        toggleActions: "play reverse play reverse",
-        // toggleClass: "active",
-    }
+    scrollTrigger: animTrigger,
 });
+
+
+
+// gsap.fromTo('.main-3s__icon-1', { scale: 1 }, {
+//     scale: 10,
+//     duration: 1,
+//     ease: "power2.out",
+//     scrollTrigger: {
+//         trigger: '.main-3s__trigger-1',
+//         // animation: animationCard,
+//         scroller: ".scroll-wrapper",
+//         start: 'top 53%',
+//         end: 'bottom 47%',
+//         ease: "power4.out",
+//         markers: true,
+//         toggleActions: "play reverse play reverse",
+//         // toggleClass: "active",
+//     }
+// });
