8e33ef76f2c9009c0774d224e7c0c2d12ae2e1a2
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-06-23 08:41:41 +0300

    anim mor elem 0.37

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -1,37 +1,43 @@
-// надо поставить переменную и куери селектор для каждого класса
+let textContainer, trigger, number, icon, heading
 
+// надо поставить переменную и куери селектор для каждого класса
+textContainer = document.querySelectorAll('.main-3s__text-container')[0];
+trigger = document.querySelectorAll('.main-3s__trigger')[0];
+number = document.querySelectorAll('.main-3s__number')[0];
+icon = document.querySelectorAll('.main-3s__icon')[0];
+heading = document.querySelectorAll('.main-3s__head-wrapper')[0];
 
 // on enter
-gsap.to('.main-3s__text-container-1',{color: "#FFEE50", duration: .4, ease: "power3.inOut",
+gsap.to(textContainer,{color: "#FFEE50", duration: .4, ease: "power3.inOut",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
     markers: true,
     toggleActions: "play reset reset reset",
 },});
-gsap.to('.main-3s__number-1',{opacity: 1, delay: .1, duration: .3, ease: "power3.out",
+gsap.to(number,{opacity: 1, delay: .1, duration: .3, ease: "power3.out",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
     markers: true,
     toggleActions: "play reset reset reset",
 },});
-gsap.fromTo(".main-3s__icon-1", {xPercent: 0}, {xPercent: 100, duration: .6, ease: "power3.inOut",
+gsap.fromTo(icon, {xPercent: 0}, {xPercent: 100, duration: .6, ease: "power3.inOut",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
     markers: true,
     toggleActions: "play reset reset reset",
 },});
-gsap.fromTo(".main-3s__head-wrapper-1", {yPercent: 0}, {yPercent: -100, duration: 1, ease: "power3.inOut",
+gsap.fromTo(heading, {yPercent: 0}, {yPercent: -100, duration: 1, ease: "power3.inOut",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
@@ -40,36 +46,36 @@ scrollTrigger: {
 },});
 
 // on leave
-gsap.to('.main-3s__text-container-1',{color: "#FFFFFF", duration: .4, ease: "power3.inOut",
+gsap.to(textContainer,{color: "#FFFFFF", duration: .4, ease: "power3.inOut",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
     markers: true,
     toggleActions: "reset play reset reset",
 },});
-gsap.to('.main-3s__number-1',{opacity: 0, delay: .1, duration: .3, ease: "power3.out",
+gsap.to(number,{opacity: 0, delay: .1, duration: .3, ease: "power3.out",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
     markers: true,
     toggleActions: "reset play reset reset",
 },});
-gsap.to(".main-3s__icon-1", {xPercent: 100, duration: .6, ease: "power3.inOut",
+gsap.to(icon, {xPercent: 100, duration: .6, ease: "power3.inOut",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
     markers: true,
     toggleActions: "reset play reset reset",
 },});
-gsap.fromTo(".main-3s__head-wrapper-1", {yPercent: 0}, {yPercent: -100, duration: 1, ease: "power3.inOut",
+gsap.fromTo(heading, {yPercent: 0}, {yPercent: -100, duration: 1, ease: "power3.inOut",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
@@ -78,27 +84,27 @@ scrollTrigger: {
 },});
 
 // on enter back
-gsap.to('.main-3s__text-container-1',{color: "#FFEE50", duration: .4, ease: "power3.inOut",
+gsap.to(textContainer,{color: "#FFEE50", duration: .4, ease: "power3.inOut",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
     markers: true,
     toggleActions: "reset reset play reset",
 },});
-gsap.to('.main-3s__number-1',{opacity: 1, delay: .1, duration: .3, ease: "power3.out",
+gsap.to(number,{opacity: 1, delay: .1, duration: .3, ease: "power3.out",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
     markers: true,
     toggleActions: "reset reset play reset",
 },});
-gsap.to(".main-3s__icon-1", {xPercent: 100, duration: .6, ease: "power3.inOut",
+gsap.to(icon, {xPercent: 100, duration: .6, ease: "power3.inOut",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
@@ -107,27 +113,27 @@ scrollTrigger: {
 },});
 
 // on leave back
-gsap.to('.main-3s__text-container-1',{color: "#FFFFFF", duration: .4, ease: "power3.inOut",
+gsap.to(textContainer,{color: "#FFFFFF", duration: .4, ease: "power3.inOut",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
     markers: true,
     toggleActions: "reset reset reset play",
 },});
-gsap.to('.main-3s__number-1',{opacity: 0, delay: .1, duration: .3, ease: "power3.out",
+gsap.to(number,{opacity: 0, delay: .1, duration: .3, ease: "power3.out",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
     markers: true,
     toggleActions: "reset reset reset play",
 },});
-gsap.to(".main-3s__icon-1", {xPercent: 200, duration: .6, ease: "power3.inOut",
+gsap.to(icon, {xPercent: 200, duration: .6, ease: "power3.inOut",
 scrollTrigger: {
-    trigger: '.main-3s__trigger-1',
+    trigger: trigger,
     scroller: ".scroll-wrapper",
     start: 'top 53%',
     end: 'bottom 47%',
