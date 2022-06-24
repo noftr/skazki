8e9b992835b965fc142135f2fc819e36529dec34
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-06-24 23:01:05 +0300

    add img animation

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -1,9 +1,7 @@
 let textContainer, trigger, number, icon, headContainer, headWrapper, iconWrapper
+const nationalities = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
 
-
-const food = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
-
-food.forEach((item, index) => {
+nationalities.forEach((item, index) => {
 
     textContainer = document.querySelectorAll('.main-3s__text-container')[index];
     trigger = document.querySelectorAll('.main-3s__trigger')[index];
@@ -11,90 +9,107 @@ food.forEach((item, index) => {
     icon = document.querySelectorAll('.main-3s__icon')[index];
     iconWrapper = document.querySelectorAll('.main-3s__icon-wrapper')[index];
     headContainer = document.querySelectorAll('.main-3s__head-container')[index];
-    headWrapper = document.querySelectorAll('.main-3s__head-wrapper')[index];
+    // headWrapper = document.querySelectorAll('.main-3s__head-wrapper')[index];
+    photo = document.querySelectorAll('.main-3s__sticky-img')[index];
 
     // on enter
+    gsap.to(photo,{opacity: 1, duration: 0.001, ease: 'none',
+        scrollTrigger: {
+            trigger: trigger,
+            scroller: ".scroll-wrapper",
+            start: 'top center',
+            end: 'bottom center',
+            toggleActions: "restart none restart none",
+        },});
     gsap.to(textContainer,{color: "#FFEE50", duration: .6, ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: trigger,
-        scroller: ".scroll-wrapper",
-        start: 'top center',
-        end: 'bottom center',
-        toggleActions: "restart none restart none",
-    },});
+        scrollTrigger: {
+            trigger: trigger,
+            scroller: ".scroll-wrapper",
+            start: 'top center',
+            end: 'bottom center',
+            toggleActions: "restart none restart none",
+        },});
     gsap.to(number,{opacity: 1, duration: .6, ease: "power2.out",
-    scrollTrigger: {
-        trigger: trigger,
-        scroller: ".scroll-wrapper",
-        start: 'top center',
-        end: 'bottom center',
-        toggleActions: "play reset play reset",
-    },});
+        scrollTrigger: {
+            trigger: trigger,
+            scroller: ".scroll-wrapper",
+            start: 'top center',
+            end: 'bottom center',
+            toggleActions: "play reset play reset",
+        },});
     gsap.to(number,{color: "#FFEE50", delay: .3, duration: .6, ease: "power2.out",
-    scrollTrigger: {
-        trigger: trigger,
-        scroller: ".scroll-wrapper",
-        start: 'top center',
-        end: 'bottom center',
-        toggleActions: "restart none restart none",
-    },});
+        scrollTrigger: {
+            trigger: trigger,
+            scroller: ".scroll-wrapper",
+            start: 'top center',
+            end: 'bottom center',
+            toggleActions: "restart none restart none",
+        },});
     gsap.to(icon, {xPercent: 100, duration: .6, ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: trigger,
-        scroller: ".scroll-wrapper",
-        start: 'top center',
-        end: 'bottom center',
-        toggleActions: "restart none restart none",
-    },});
+        scrollTrigger: {
+            trigger: trigger,
+            scroller: ".scroll-wrapper",
+            start: 'top center',
+            end: 'bottom center',
+            toggleActions: "restart none restart none",
+        },});
     gsap.to(headContainer, {yPercent: -100, duration: .6, ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: trigger,
-        scroller: ".scroll-wrapper",
-        start: 'top center',
-        end: 'bottom center',
-        toggleActions: "restart none restart none",
-    },});
+        scrollTrigger: {
+            trigger: trigger,
+            scroller: ".scroll-wrapper",
+            start: 'top center',
+            end: 'bottom center',
+            toggleActions: "restart none restart none",
+        },});
 
     // on leave
+    gsap.to(photo,{opacity: 0, duration: 0.001, ease: 'none',
+        scrollTrigger: {
+            trigger: trigger,
+            scroller: ".scroll-wrapper",
+            start: 'top center',
+            end: 'bottom center',
+            toggleActions: "reset play reset play",
+        },});
     gsap.to(textContainer,{color: "#FFFFFF", duration: .6, ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: trigger,
-        scroller: ".scroll-wrapper",
-        start: 'top center',
-        end: 'bottom center',
-        toggleActions: "reset play reset play",
-    },});
+        scrollTrigger: {
+            trigger: trigger,
+            scroller: ".scroll-wrapper",
+            start: 'top center',
+            end: 'bottom center',
+            toggleActions: "reset play reset play",
+        },});
     gsap.to(number,{opacity: 0, duration: .6, ease: "power2.out",
-    scrollTrigger: {
-        trigger: trigger,
-        scroller: ".scroll-wrapper",
-        start: 'top center',
-        end: 'bottom center',
-        toggleActions: "reset play reset play",
-    },});
+        scrollTrigger: {
+            trigger: trigger,
+            scroller: ".scroll-wrapper",
+            start: 'top center',
+            end: 'bottom center',
+            toggleActions: "reset play reset play",
+        },});
     gsap.to(number,{color: "#FFFFFF", delay: .3, duration: .6, ease: "power2.out",
-    scrollTrigger: {
-        trigger: trigger,
-        scroller: ".scroll-wrapper",
-        start: 'top center',
-        end: 'bottom center',
-        toggleActions: "reset play reset play",
-    },});
+        scrollTrigger: {
+            trigger: trigger,
+            scroller: ".scroll-wrapper",
+            start: 'top center',
+            end: 'bottom center',
+            toggleActions: "reset play reset play",
+        },});
     gsap.to(iconWrapper,{xPercent: 100, duration: .6, ease: "power2.inOut",
-    scrollTrigger: {
-        trigger: trigger,
-        scroller: ".scroll-wrapper",
-        start: 'top center',
-        end: 'bottom center',
-        toggleActions: "reset play reset play",
-    },});
+        scrollTrigger: {
+            trigger: trigger,
+            scroller: ".scroll-wrapper",
+            start: 'top center',
+            end: 'bottom center',
+            toggleActions: "reset play reset play",
+        },});
     // gsap.to(headWrapper, {yPercent: -100, duration: 1, ease: "power2.inOut",
-    // scrollTrigger: {
-    //     trigger: trigger,
-    //     scroller: ".scroll-wrapper",
-    //     start: 'top center',
-    //     end: 'bottom center',
-    //     toggleActions: "none restart none restart",
-    // },});
+    //     scrollTrigger: {
+    //         trigger: trigger,
+    //         scroller: ".scroll-wrapper",
+    //         start: 'top center',
+    //         end: 'bottom center',
+    //         toggleActions: "none restart none restart",
+    //     },});
 
 })
