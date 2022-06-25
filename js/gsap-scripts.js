65057e197a0e2c951f97bd54368c316beea2246d
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-06-25 11:46:16 +0300

    1

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -118,6 +118,7 @@ nationalities.forEach((item, index) => {
     // ругается что внутри не работает
     textContainer.addEventListener("click", scrollToEl);
     function scrollToEl() {
-        anchor.scrollIntoView();
+        scroll.scrollTo(anchor, duration: 1000)
+        // anchor.scrollIntoView();
     };
 })
