4be611ba14b2d86fbe2e9a7bd03ea40831bc34e6
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-06-25 19:10:52 +0300

    1

--- js/gsap-scripts.js
+++ js/gsap-scripts.js
@@ -153,7 +153,7 @@ function initLocomotiveScroll() {
 
       anchorArea.addEventListener("click", scrollToEl);
       function scrollToEl() {
-          locomotiveScroll.scrollTo(document.querySelectorAll('.main-3s__anchor')[index]);
+          locomotiveScroll.scrollTo(document.querySelectorAll('.main-3s__anchor')[index], duration: 500);
       };
   })
 
